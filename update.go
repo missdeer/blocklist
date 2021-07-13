a097a92608555010496c617df8f5eab2da4e40c6
Fan Yang <me@minidump.info>
2021-07-13 09:41:58 +0800

    don't check existent, always add the domain name

--- update.go
+++ update.go
@@ -263,14 +263,9 @@ func main() {
 	}
 	wg.Wait()
 
-	// remove non-exist domain names
-	go receiveDomains()
-	wg.Add(len(domains))
 	for domain := range domains {
-		sema.Acquire()
-		go checkExistent(domain, &wg)
+		finalDomains[domain] = struct{}{}
 	}
-	wg.Wait()
 	quit <- true
 
 	// handle domain names of short URL services
